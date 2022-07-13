f384d10b5b4b0d5f5f961c6d80ded4e990030e15
George Fahmy <george@kespry.com>
2022-07-12 20:04:12 -0700

    move make_database to a function

--- utils/make_database.py
+++ utils/make_database.py
@@ -1,31 +1,33 @@
 import sqlite3 as s
 
-create_meals_table = """
-    CREATE TABLE IF NOT EXISTS meals
-    (
-        id integer PRIMARY KEY,
-        meal text NOT NULL,
-        ingredients text,
-        recipe_link text
-    );
-"""
 
-create_plan_table = """
-    CREATE TABLE IF NOT EXISTS plans
-    (
-        id integer PRIMARY KEY,
-        week_date text NOT NULL UNIQUE,
-        meals text NOT NULL,
-        ingredients text
-    );
-"""
+def make_database(database_name="meals_database.db"):
+    create_meals_table = """
+        CREATE TABLE IF NOT EXISTS meals
+        (
+            id integer PRIMARY KEY,
+            meal text NOT NULL,
+            ingredients text,
+            recipe_link text
+        );
+    """
 
-conn = s.connect("meals_database.db")
-c = conn.cursor()
+    create_plan_table = """
+        CREATE TABLE IF NOT EXISTS plans
+        (
+            id integer PRIMARY KEY,
+            week_date text NOT NULL UNIQUE,
+            meals text NOT NULL,
+            ingredients text
+        );
+    """
 
-if conn:
-    c.execute(create_meals_table)
-    c.execute(create_plan_table)
-    conn.commit()
-else:
-    print("Error")
+    conn = s.connect(database_name)
+    c = conn.cursor()
+
+    if conn:
+        c.execute(create_meals_table)
+        c.execute(create_plan_table)
+        conn.commit()
+    else:
+        print("Error")
