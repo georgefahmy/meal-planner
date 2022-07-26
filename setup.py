713ef65289039589c48d43c19abce0b2e2f8df31
George Fahmy <geofahm@gmail.com>
2022-07-26 09:52:58 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Usage:
     python setup.py py2app
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 APP = ["planner_app.py"]
 DATA_FILES = [
@@ -20,12 +20,15 @@ OPTIONS = {"iconfile": "/Users/GFahmy/Documents/projects/meal-planner/resources/
 setup(
     app=APP,
     name="Meal Planner PRO",
+    url="https://github.com/georgefahmy/meal-planner",
     data_files=DATA_FILES,
     options={"py2app": OPTIONS},
     setup_requires=["py2app", "PySimpleGUI", "altgraph", "modulegraph", "macholib"],
+    packages=find_packages(),
     version="3.0.0",
     author="George Fahmy",
     description="Meal Planner PRO",
+    python_requires=">=3.6",
     long_description="""Meal Planner PRO is a one stop shop for all your meal planning. You can add
         all the meals and food you enjoy and lvoe into your database, including all the ingredients
         to make that food. From there, you can build out meal plans, and shopping lists, and even
