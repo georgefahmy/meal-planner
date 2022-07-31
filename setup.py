ddd55d1d36fad7950cdba292b8c976e355c8a4e3
George Fahmy <geofahm@gmail.com>
2022-07-30 23:19:35 -0700

    Pick date bug (#10)
    * fixed bug with the pick date button not actually using the date picked
    
    * bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "recipe-scrapers",
     ],
     packages=find_packages(),
-    version="3.2.0",
+    version="3.2.1",
     author="George Fahmy",
     description="Meal Planner PRO",
     python_requires=">=3.6",
