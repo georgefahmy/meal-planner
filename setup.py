aa16874d112a4b3c696d228956fdbca760535489
George Fahmy <geofahm@gmail.com>
2022-07-30 23:48:26 -0700

    Viewer updates (#12)
    * minor changes to the recipe viewer to better handle longer recipe ingredients
    
    * bump version and changelog

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "recipe-scrapers",
     ],
     packages=find_packages(),
-    version="3.2.2",
+    version="3.2.3",
     author="George Fahmy",
     description="Meal Planner PRO",
     python_requires=">=3.6",
