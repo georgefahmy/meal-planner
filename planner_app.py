c781d3a7af3f12b56bd613eec5da862ab18ac031
George Fahmy <geofahm@gmail.com>
2022-11-10 12:47:18 -0800

    updates

--- planner_app.py
+++ planner_app.py
@@ -268,7 +268,14 @@ right_column = [
                         select_mode=sg.LISTBOX_SELECT_MODE_SINGLE,
                     )
                 ],
-                [sg.Button("View Recipe", key="-VIEW_RECIPE-",)],
+                [
+                    sg.Button(
+                        "View Recipe",
+                        key="-VIEW_RECIPE-",
+                        disabled=True,
+                        disabled_button_color=("black", "gray"),
+                    )
+                ],
             ],
         ),
     ],
